c0cd84cbe64dd9386b02cbd7a43fa07eb21e073f
faruano <94489074+faruano@users.noreply.github.com>
2023-04-04 22:56:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 761791964 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    def solution(x: np.array) -> float:
+    lamb_da = (np.mean(x)*2)/(8100)
+    return lamb_da # Ваш ответ
